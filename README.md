71626e1cd19f2971c66452cd8c4c9077b7da43fa
Chris Geirman <chris@geirman.com>
2017-04-16 13:52:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Serverless Stack Tutorial
+These files represent my completed app after following the VERY GOOD tutorial at http://serverless-stack.com/. I highly recommend it.
+
+The completed app can be viewed at the following two urls...
+- http://notes-app-client-geirman.s3-website-us-east-1.amazonaws.com
+- http://kikitty.com
---
+++ client/src/config.js
@@ -0,0 +1,15 @@
+export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    cognito: {
+        USER_POOL_ID: 'us-east-2_dUMVe5SZr',
+        APP_CLIENT_ID: '2tu8ihu6sqkakoqgspn3v9be31',
+        REGION: 'us-east-2',
+        IDENTITY_POOL_ID: 'us-east-2:bde2d90d-9ec1-4769-af49-4cd97a2316e4',
+    },
+    s3: {
+        BUCKET: 'notes-tutorial-app-geirman'
+    },    
+    apiGateway: {
+        URL: 'https://819pxkk94e.execute-api.us-east-2.amazonaws.com/prod',
+    }
+}
